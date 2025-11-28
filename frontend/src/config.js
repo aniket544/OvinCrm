9fd4d4056c50d9d7d125831900aba144a08418a9
aniket544 <tuffan444max@gmail.com>
2025-11-28 11:18:41 +0530

    change the config url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 // Yahan apna Render wala Backend URL daal (last mein slash '/' mat lagana)
-const BASE_API_URL = "https://my-crm-backend.onrender.com";
+const BASE_API_URL = "https://my-crm-backend-a5q4.onrender.com";
 
 export default BASE_API_URL;
