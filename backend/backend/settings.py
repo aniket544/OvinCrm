84a42500e08681519bbdc07bbf7ec1d78340a13d
aniket544 <tuffan444max@gmail.com>
2025-12-08 17:22:48 +0530

    change server time to 0

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:///db.sqlite3',
-        conn_max_age=600
+        conn_max_age=0
     )
 }
 
